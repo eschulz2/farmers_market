750048f50f0589dadd17e9ae9aa6e160ae698dd1
Eric Schulz <eschulz2@gmail.com>
2014-01-21 17:43:14 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FarmersMarket::Application.config.secret_token = '23a6ac95da98c3b10b985cdd194f79e5ef7c9879341db4ad7f068d09561fdbeb27e0c2c32abdeaed10ce0fbcaa78378efcf4b7fcad78edfbe8e73e9bdd5a94ca'
